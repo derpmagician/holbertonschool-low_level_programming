6ec279036bf50314ee47bac5d5b0fed0ccb8f2dc
Renzo <renzin@gmail.com>
2021-02-04 17:35:33 +0000

    adding ex 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Description
+ *
+ * Description:putchar all numbers from 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%i", n);
+	putchar('\n');
+	return (0);
+}i
