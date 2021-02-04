652d07157bf23487fe00f850f5d7aba20726259d
Renzo <renzin@gmail.com>
2021-02-04 17:27:44 +0000

    creating ex 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Description
+ * Output letters with for loop
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
