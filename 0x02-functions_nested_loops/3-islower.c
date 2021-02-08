0166d72c547eeaacceefb2d040fcfe30481674f4
Renzo <renzin@gmail.com>
2021-02-08 21:51:13 +0000

    adding ex 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+nclude "holberton.h"
+/**
+ * _islower -
+ * test if lowercase
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
