825b70aa73de99ba838ce1a93a1dc320dfa92a66
Renzo <renzin@gmail.com>
2021-02-08 21:47:12 +0000

    declaring variables

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,14 @@
 
 void print_alphabet(void)
 {
-
-	for (i=0;i<9){
+	int i;
 	char c;
-
+	for (i=0;i<9)
+	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
-	c='a'
 	}
 }
