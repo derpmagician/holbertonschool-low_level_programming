c7b4b67db94766ac5557134c94526aff5f973fe7
Renzo <renzin@gmail.com>
2021-02-08 17:33:50 +0000

    corregir codigo

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,8 +5,17 @@
  * Peinrs Holberton and a new line
  * Return: 0
  */
-int main(void){
+int main(void)
+{
+	int i = 0;
+	char c;
+	char l[] = "Holberton\n";
 
-	printf("Holberton\n")
-	return(0)
+	while (i <= 9)
+	{
+		c = l[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
 }
