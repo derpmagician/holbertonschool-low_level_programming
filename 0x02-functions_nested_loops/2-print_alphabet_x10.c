9727af103a5b9613bdcb2483ede73cbcd69e16ce
Renzo <renzin@gmail.com>
2021-02-08 21:42:07 +0000

    adding ex 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet with _putchar x10
+ *
+ * Return: void
+ */
+
+
+void print_alphabet(void)
+{
+
+	for (i=0;i<9){
+	char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	c='a'
+	}
+}
