c23e496147b4614fbdc3d337e07291fdce5d49d2
Renzo <renzin@gmail.com>
2021-02-08 17:25:29 +0000

    agregar carpeta

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main - Description
+ * Peinrs Holberton and a new line
+ * Return: 0
+ */
+int main(void){
+
+	printf("Holberton\n")
+	return(0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Ejercicios de Holberton 0x02. C - Functions, nested loops
