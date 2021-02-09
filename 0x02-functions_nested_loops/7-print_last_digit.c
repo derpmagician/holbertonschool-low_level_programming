5dfb6dd2a1ba9a85b089558a7f0facaec4d5470f
Renzo <renzin@gmail.com>
2021-02-09 14:58:05 +0000

    fixing description

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit - description
  * @n: digit to find
- * Return: The last digit.
+ * Return: last digit.
  */
 int print_last_digit(int n)
 {
@@ -11,3 +11,4 @@ int print_last_digit(int n)
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
+
