7a8d635ac5f14015044ae7c883fd88e4938f1d1a
Renzo <renzin@gmail.com>
2021-02-11 15:33:00 +0000

    adding ex 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - return de value of multiplication between a and b
+ * @a: input one
+ * @b: input two
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
