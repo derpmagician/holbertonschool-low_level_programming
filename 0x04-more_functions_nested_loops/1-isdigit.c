a784de8a206964a3dd131c7e7b7dc566b4a9a6a3
Renzo <renzin@gmail.com>
2021-02-11 17:51:04 +0000

    changin values

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 nclude "holberton.h"
 /**
-*_isdigit - Return 1 if c is a digit.
-*@c: Value
-*Return: 1 if c is a value ,0 otherwise
+* _isdigit - Return 1 if c is a digit.
+* @c: Value
+* Return: 1 if value, else 0
 */
 int _isdigit(int c)
 {
-	if (c >= 47 && c <= 57)
+	if (c >= '0' && c <= '9')
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
