90bf1f72848f015325bccf7568775dc1018267e3
Renzo <renzin@gmail.com>
2021-02-11 15:41:23 +0000

    adding ex 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints 0 to 9
+ * Return: void
+ */
+void print_numbers(void);
+{
+	int c;
+
+	for (c = "0"; < "10"; c++)
+		_putchar(c);
+	_putchar("\n")
+}
