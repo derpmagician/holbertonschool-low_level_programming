1ce1ca04a545f18bf7f11bfb60b89060ff38c799
Renzo <renzin@gmail.com>
2021-02-11 21:59:27 +0000

    adding ex 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Fizz Buzz.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i <= 1; i >= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			pritnf("Fizz");
+		}
+		else if ((i % 5) == 0 && (i % 3) != 0)
+		{
+			pritnf("Buzz");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			pritnf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+}
