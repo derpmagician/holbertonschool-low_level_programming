fed9de0ba32806896daf53c41ce652604a7187a0
Renzo <renzin@gmail.com>
2021-02-11 21:12:07 +0000

    fixing code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ void more_numbers(void)
 
 	for (c= 0; c <=9; c++)
 	{
-	for (i = 0; i <= 14; i++)
-	{
-		if (i > 9)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
-	}
-	_putchar('\n');
-	}
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+		}
 }
