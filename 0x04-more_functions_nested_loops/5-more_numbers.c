08f4972af556107583f53d9da4ed0de0b0cb7485
Renzo <renzin@gmail.com>
2021-02-11 19:53:36 +0000

    adding ex 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - prints numbers, from 0 to 14, 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+			_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+}
