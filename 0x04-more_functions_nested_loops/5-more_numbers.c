a4711f6b7a6760d038cb7f7c8eb68ec515af9d52
Renzo <renzin@gmail.com>
2021-02-11 20:58:22 +0000

    fixing code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i;
-
-	for (i = 0; i <= 14; i++)
+	int i count;
+	
+	for (count = 0; count>=9; count++)
 	{
-		if (i > 9)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 	}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	while(n>0)
+		_putchar()
+
+	_putchar('\n')
+}
