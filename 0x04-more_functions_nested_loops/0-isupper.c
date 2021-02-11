f4f1a1d529178694d988e9d0e59dd485845bb5e8
Renzo <renzin@gmail.com>
2021-02-11 17:21:09 +0000

    fixing errors

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
-
+include "holberton.h"
 /**
- * _isupper - Entry point
- *@c: charater to check
- * Return: 1 for uppercase, 0 for lowercase
- */
+* _isupper - Return 1 if c is uppercase.
+*@c: value to return
+*Return: 0 or 1 .
+*/
 int _isupper(int c)
 {
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
 
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+return (0);
 }
