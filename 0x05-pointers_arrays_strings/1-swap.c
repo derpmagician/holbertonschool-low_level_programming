e5005ecec6cd61320cb7bed18d987024e9caaf91
Renzo <renzin@gmail.com>
2021-02-16 14:56:32 +0000

    adding ex 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap - swaps the values of two integers.
+ * @a: integer to swap
+ * @b: integer to swap
+ * Resturn: void
+ */
+void  swap(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	b = *a;
+	a = *n;
+}
