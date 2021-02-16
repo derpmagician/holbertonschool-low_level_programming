dc967bba0f7479527cc7b46ea324467e93909b2a
Renzo <renzin@gmail.com>
2021-02-16 16:08:25 +0000

    adding ex 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * int _strlen - measures the lenght of *s
+ * @s: string to be meassure
+ * Resturn: lenght of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(*s[i] != '\0')
+	{
+		i++;
+	}
+	
+
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _puts - swaps the values of two integers.
+ * @a: integer to swap
+ * @b: integer to swap
+ * Resturn: void
+ */
+void _puts(char *str)
+{
+	int n;
+
+}
