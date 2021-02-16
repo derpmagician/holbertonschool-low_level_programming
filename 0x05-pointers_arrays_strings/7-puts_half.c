ad3b4c0d342fd1ba85d23fba3e4b9e34dd6d8559
Renzo <renzin@gmail.com>
2021-02-16 21:44:21 +0000

    fixing code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
-
 /**
  * puts_half - Prints second half of a string
  * @str: String which second half will be printed
  */
 void puts_half(char *str)
 {
+	int len = 0;
 	int n;
-	int largo;
-	int inicio;
 
-	len = 0;
-
-	while (*(str + largo) != '\0')
-		largo++;
-
-	inicio = largo / 2;
-
-	if (largo % 2 != 0)
-		inicio++;
-
-	for (n = inicio; n < largo; n++)
-		_putchar(str[n]);
+	while (str[len] != 0)
+		len++;
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+	while (n > 0)
+	{
+		_putchar(str[len - n]);
+		n--;
+	}
 
 	_putchar('\n');
 }
