ff1cb743e46b36d701d2f01778863c5bdb7ff8bd
Renzo <renzin@gmail.com>
2021-02-17 20:05:01 +0000

    adding file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void substract_key(char key[], int len, int checksum);
+
+/**
+ * main - Generates a random password
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+}
