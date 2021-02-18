eccfc1838b75bce866a6700eb91d6ca69db20955
Renzo <renzin@gmail.com>
2021-02-18 20:39:04 +0000

    implementing Betty requeriments

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * reverse_array  - Reverse strings
  * @a: first string
@@ -13,6 +12,7 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 	int b[1000];
+
 	for (j = 0; j <= n; j++)
 	b[j] = a[j];
 	j = 0;
