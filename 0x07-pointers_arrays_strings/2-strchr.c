92e3854444e4373e74e6b1a41c3d759ed0a7b8e5
Renzo <renzin@gmail.com>
2021-02-22 19:45:00 +0000

    adding ex

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - Takes a pointer to an int
+ * @s: destination string
+ * @c: source string
+ *
+ * Description: Appends two strings
+ * Return: char
+*/
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+	if (*s == c)
+	{
+		return (s);
+	}
+	s++;
+}
+if (*s == c)
+	return (s);
+else
+	return ('\0');
+}
