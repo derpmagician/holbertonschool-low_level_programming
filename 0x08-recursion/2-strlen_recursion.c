ee343f6cb29dfc008ac7986ca1eb84a8c20339c9
Renzo <renzin@gmail.com>
2021-02-24 18:04:53 +0000

    addding ex 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * _strlen_recursion - Calculate the length of a string
+  * @s: the string to count
+  *
+  * Return: integer value
+  */
+int _strlen_recursion(char *s)
+{
+	while(*s)
+	{
+		s++;
+		return(_strlen_recursion(s)+1);
+	}
+}
