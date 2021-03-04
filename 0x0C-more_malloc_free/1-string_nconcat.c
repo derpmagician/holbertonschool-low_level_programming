9b51077d2bfdf6463d17894580a736d20752208e
Renzo <renzin@gmail.com>
2021-03-04 21:59:09 +0000

    following Betty standards

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,42 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int ls1 = 0, ls2 = 0, x = 0, l = 0;
-    char *str;
+	unsigned int ls1 = 0, ls2 = 0, x = 0, l = 0;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[ls1])
-        ls1++;
+	while (s1[ls1])
+		ls1++;
 
-    while (s2[ls2])
-        ls2++;
+	while (s2[ls2])
+		ls2++;
 
-    if (n >= ls2)
-        l = ls1 + ls2;
-    else
-        l = ls1 + n;
+	if (n >= ls2)
+		l = ls1 + ls2;
+	else
+		l = ls1 + n;
 
-    str = malloc(sizeof(char) * l + 1);
-    if (str == NULL)
-        return (NULL);
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
 
-    ls2 = 0;
-    while (x < l)
-    {
-        if (x <= ls1)
-            str[x] = s1[x];
+	ls2 = 0;
+	while (x < l)
+	{
+		if (x <= ls1)
+			str[x] = s1[x];
 
-        if (x >= ls1)
-        {
-            str[x] = s2[ls2];
-            ls2++;
-        }
-        x++;
-    }
-    str[x] = '\0';
-    return (str);
+		if (x >= ls1)
+		{
+			str[x] = s2[ls2];
+			ls2++;
+		}
+		x++;
+	}
+	str[x] = '\0';
+	return (str);
 }
