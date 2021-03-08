01133998c93cf8ab3e81bfe2c6839ae3750913c6
Renzo <renzin@gmail.com>
2021-03-08 20:29:12 +0000

    adding typedef structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef foir dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
