82a8f7a32371d8343c5a5949c720dcfcacd1aad8
Renzo <renzin@gmail.com>
2021-03-23 15:50:40 +0000

    adding ex 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
 #include "lists.h"
-#include <stdio.h>
 
 /**
   * get_nodeint_at_index - returns the nth node of a listint_t linked list.
@@ -10,19 +8,12 @@
   */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count = 0;
+	unsigned int i = 0;
 
-	if (head)
+	while (head && (i != index))
 	{
-		while (head)
-		{
-			if (count == index)
-				return (head);
-
-			head = head->next;
-			count++;
-		}
+		head = head->next;
+		i++;
 	}
-
-	return (NULL);
+	return (head);
 }
