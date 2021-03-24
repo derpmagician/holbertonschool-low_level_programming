afee4514cef61c173da661bfb1b48c9c37531c6c
Renzo <renzin@gmail.com>
2021-03-24 03:51:36 +0000

    fixing the broken code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,17 +9,21 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	int count = 0;
-	const listint_t *sel;
-
-	sel = head;
+	const listint_t *sel, *slow, *fast;
 
 	if (head == NULL)
 		return (0);
+
+	sel = head;
+	slow = fast = head;
+
 	while (curr != NULL)
 	{
 		printf("[p] %d\n" (void *)sel, sel->n);
 		sel = sel->next;
 		count++;
+
+		slow = slow->next;
 	}
 	return (count);
 }
