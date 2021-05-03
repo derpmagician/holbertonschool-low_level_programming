ae9ce8671fbc99ce400787b85e63008704ed50a9
Renzo <renzin@gmail.com>
2021-05-03 14:19:09 +0000

    adding comments

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,20 +1,26 @@
-#ifndef _DYN_LIBS_
-#define _DYN_LIBS_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _putchar(char c);
+int _putchar(char);
+
+/* 0x02 */
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n);
+int _abs(int);
+
+/* 0x04 */
 int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
+
+/* 0x05 */
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+
+/* 0x06 */
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+
+/* 0x07 */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -22,4 +28,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HOLBERTON_H */
