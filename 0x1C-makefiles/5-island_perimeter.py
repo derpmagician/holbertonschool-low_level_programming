d6342444a548439a88f2c54b471834f43f0a4691
derpmagician <renzin@gmail.com>
2021-07-02 12:38:32 -0500

    erasing white space

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,15 +13,15 @@ def island_perimeter(grid):
             if cell == LAND:
                 # print("land in [x= {:d} y= {:d}]".format(x, y))
                 # left
-                if y == 0 or grid[y - 1][x] == 0:
+                if y == 0 or grid[y - 1][x] == WATER:
                     perimeter += 1
                 # right
-                if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                if y == len(grid) - 1 or grid[y + 1][x] == WATER:
                     perimeter += 1
                 # up
-                if x == 0 or grid[y][x - 1] == 0:
+                if x == 0 or grid[y][x - 1] == WATER:
                     perimeter += 1
                 # down
-                if x == len(row) - 1 or grid[y][x + 1] == 0:
+                if x == len(row) - 1 or grid[y][x + 1] == WATER:
                     perimeter += 1                
     return perimeter
