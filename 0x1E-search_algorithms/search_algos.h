acd4278e925593c3ea178895a79637c83edd5c7f
derpmagician <renzin@gmail.com>
2021-08-29 22:55:32 -0500

    [ADD] file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _ALG_H_
+#define _ALG_H_
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int aaa(int *array, size_t size, int value, int l, int h);
+
+
+
+#endif /* _ALG_H_ */
