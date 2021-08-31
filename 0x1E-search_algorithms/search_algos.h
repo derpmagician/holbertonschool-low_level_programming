1af45c558dccfc07a9c2786175ef4a5662c74e81
derpmagician <renzin@gmail.com>
2021-08-31 12:06:07 -0500

    [UPDATE] new algort

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,16 @@
-#ifndef _ALG_H_
-#define _ALG_H_
+#ifndef _S_ALG_H_
+#define _S_ALG_H_
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int aaa(int *array, size_t size, int value, int l, int h);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 
-
-#endif /* _ALG_H_ */
+#endif /* _S_ALG_H_ */
