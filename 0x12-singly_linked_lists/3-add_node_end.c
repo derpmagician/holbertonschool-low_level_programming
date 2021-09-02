ea6a049c0e36d4f4310c6fad5fe0f0ca2750150a
derpmagician <renzin@gmail.com>
2021-09-01 19:59:28 -0500

    [UPDATE] identation fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,29 +16,26 @@ list_t *new;
 list_t *last;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
+
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
-	return (new);
+		return (new);
 	}
 	last = *head;
 	while (last->next != NULL)
-	{
 		last = last->next;
-	}
+
 	last->next = new;
-return (new);
+	return (new);
 }
 /**
 * _strlen - Returns the length of a string.
@@ -48,12 +45,10 @@ return (new);
 */
 int _strlen(const char *s)
 {
-int i;
-i = 0;
+	int i = 0;
 
 	while (s[i] != 0)
-	{
-	i++;
-	}
-return (i);
+		i++;
+
+	return (i);
 }
